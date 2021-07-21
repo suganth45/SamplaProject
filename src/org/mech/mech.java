2fe3238bef5e240f7842bf5eb581b1c37bc031da
suganth45 <suganth456@gmail.com>
2021-07-21 09:32:37 +0530

    Suvanesh push

---
+++ src/org/mech/mech.java
@@ -0,0 +1,28 @@
+package org.mech;
+
+public class mech {
+			public static void main(String[] args)
+		{
+		
+			String studentid ="mkce/063";
+			String studentName="sugan";
+			String studentEmail="abc@gmail.com";
+			long studentphno=1234567890l;
+			String studentDept ="Mechanical";
+			char studentgender ='M';
+			String studentcity="chennai";
+			
+			System.out.println("emp id     is   "+studentid);
+			System.out.println("empName    is   "+studentName);
+			System.out.println("Email      is   "+studentEmail);
+			System.out.println("phno       is   "+studentphno);
+			System.out.println("department is   "+studentDept);
+			System.out.println("Gender     is   "+studentgender);
+			System.out.println("stdntcity  is   "+studentcity);
+			
+			
+		}
+
+	}
+
+
