c420e6e07c49694807a51ed026086942ecef366e
suganth45 <suganth456@gmail.com>
2021-07-22 09:11:54 +0530

    UserName Code

--- src/org/mech/mech.java
+++ src/org/mech/mech.java
@@ -1,28 +1,31 @@
 package org.mech;
 
 public class mech {
-			public static void main(String[] args)
-		{
-		
-			String studentid ="mkce/063";
-			String studentName="sugan";
-			String studentEmail="abc@gmail.com";
-			long studentphno=1234567890l;
-			String studentDept ="Mechanical";
-			char studentgender ='M';
-			String studentcity="chennai";
-			
-			System.out.println("emp id     is   "+studentid);
-			System.out.println("empName    is   "+studentName);
-			System.out.println("Email      is   "+studentEmail);
-			System.out.println("phno       is   "+studentphno);
-			System.out.println("department is   "+studentDept);
-			System.out.println("Gender     is   "+studentgender);
-			System.out.println("stdntcity  is   "+studentcity);
-			
-			
-		}
+
+	public static void userName() {
+		// TODO Auto-generated method stub
+		System.out.println("UserName");
+	}
+
+	public static void main(String[] args) {
+
+		userName();
+		String studentid = "mkce/063";
+		String studentName = "sugan";
+		String studentEmail = "abc@gmail.com";
+		long studentphno = 1234567890l;
+		String studentDept = "Mechanical";
+		char studentgender = 'M';
+		String studentcity = "chennai";
+
+		System.out.println("emp id     is   " + studentid);
+		System.out.println("empName    is   " + studentName);
+		System.out.println("Email      is   " + studentEmail);
+		System.out.println("phno       is   " + studentphno);
+		System.out.println("department is   " + studentDept);
+		System.out.println("Gender     is   " + studentgender);
+		System.out.println("stdntcity  is   " + studentcity);
 
 	}
 
-
+}
